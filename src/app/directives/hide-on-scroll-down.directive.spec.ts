19acad355e03d16de60a33638415c87c6f147997
KrzysztofProgramming <krzychuk112@gmail.com>
2022-01-06 17:30:55 +0100

    commit poprawa

--- src/app/directives/hide-on-scroll-down.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HideOnScrollDownDirective } from './hide-on-scroll-down.directive';
-
-describe('HideOnScrollDownDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HideOnScrollDownDirective();
-    expect(directive).toBeTruthy();
-  });
-});
